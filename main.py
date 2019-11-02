ad368a344bbcc4159943826a4dac4a7ac347807a
crushyna <crushyna@gmail.com>
2019-11-02 14:04:19 +0100

    tensorflow ready, need to fill with test image

--- main.py
+++ main.py
@@ -16,39 +16,39 @@ def save():
     image1.save(image_file)
     fileName = image_file
 
-def paint(event):ss
+def paint(event):
     # python_green = "#476042"
     x1, y1 = (event.x - 1), (event.y - 1)
     x2, y2 = (event.x + 1), (event.y + 1)
     cv.create_oval(x1, y1, x2, y2, fill="black",width=5)
     draw.line([x1, y1, x2, y2],fill="black",width=5)
 
-root = Tk()
+    root = Tk()
 
-# Tkinter create a canvas to draw on
-cv = Canvas(root, width=width, height=height, bg='white')
-cv.pack()
+    # Tkinter create a canvas to draw on
+    cv = Canvas(root, width=width, height=height, bg='white')
+    cv.pack()
 
-# PIL create an empty image and draw object to draw on
-# memory only, not visible
-image1 = PIL.Image.new("RGB", (width, height), white)
-draw = ImageDraw.Draw(image1)
+    # PIL create an empty image and draw object to draw on
+    # memory only, not visible
+    image1 = PIL.Image.new("RGB", (width, height), white)
+    draw = ImageDraw.Draw(image1)
 
-# do the Tkinter canvas drawings (visible)
-# cv.create_line([0, center, width, center], fill='green')
+    # do the Tkinter canvas drawings (visible)
+    # cv.create_line([0, center, width, center], fill='green')
 
-cv.pack(expand=YES, fill=BOTH)
-cv.bind("<B1-Motion>", paint)
+    cv.pack(expand=YES, fill=BOTH)
+    cv.bind("<B1-Motion>", paint)
 
-# do the PIL image/draw (in memory) drawings
-# draw.line([0, center, width, center], green)
+    # do the PIL image/draw (in memory) drawings
+    # draw.line([0, center, width, center], green)
 
-# PIL image can be saved as .png .jpg .gif or .bmp file (among others)
-# filename = "my_drawing.png"
-# image1.save(filename)
-button1=Button(text="Save",command=save)
-button2=Button(text="Recognize", command=save)
-button1.pack(side=LEFT)
-button2.pack(side=RIGHT)
+    # PIL image can be saved as .png .jpg .gif or .bmp file (among others)
+    # filename = "my_drawing.png"
+    # image1.save(filename)
+    button1=Button(text="Save",command=save)
+    button2=Button(text="Recognize", command=save)
+    button1.pack(side=LEFT)
+    button2.pack(side=RIGHT)
 
-root.mainloop()
+    root.mainloop()
--- number_recognizer.py
+++ number_recognizer.py
@@ -18,7 +18,7 @@ def numberRecognition():
     n_output = 10   # number (digit 0-9)
 
     learning_rate = 1e-4
-    n_iterations = 1000
+    n_iterations = 1200
     batch_size = 128
     dropout = 0.5
 
@@ -47,7 +47,7 @@ def numberRecognition():
     layer_2 = tf.add(tf.matmul(layer_1, weights['w2']), biases['b2'])
     layer_3 = tf.add(tf.matmul(layer_2, weights['w3']), biases['b3'])
     layer_drop = tf.nn.dropout(layer_3, keep_prob)
-    output_layer = matmul(layer_3, weights['out']) + biases['out']
+    output_layer = tf.matmul(layer_3, weights['out']) + biases['out']
 
     ## Adam optimizer implementation
     cross_entropy = tf.reduce_mean(
@@ -56,5 +56,38 @@ def numberRecognition():
             ))
     train_step = tf.train.AdamOptimizer(learning_rate).minimize(cross_entropy)
 
+    # accuracy evaluation
+    correct_pred = tf.equal(tf.argmax(output_layer, 1), tf.argmax(Y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
+
+    # session initialization
+    init = tf.global_variables_initializer()
+    sess = tf.Session()
+    sess.run(init)
+
+    # train on mini batches
+    for i in range(n_iterations):
+        batch_x, batch_y = mnist.train.next_batch(batch_size)
+        sess.run(train_step, feed_dict={
+            X: batch_x, Y: batch_y, keep_prob: dropout
+            })
+
+    # print loss and accuracy (per minibatch)
+        if i % 100 == 0:
+            minibatch_loss, minibatch_accuracy = sess.run(
+                [cross_entropy, accuracy],
+                feed_dict={X: batch_x, Y: batch_y, keep_prob: 1.0}
+                )
+            print(
+                "Iteration",
+                str(i),
+                "\t| Loss =",
+                str(minibatch_loss),
+                "\t| Accuracy =",
+                str(minibatch_accuracy)
+                )
+    # accuracy test
+    test_accuracy = sess.run(accuracy, feed_dict={X: mnist.test.images, Y: mnist.test.labels, keep_prob: 1.0})
+    print("\nAccuracy on test set:", test_accuracy)
 
 numberRecognition()
