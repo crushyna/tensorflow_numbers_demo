78461359800ace60140dcaaf5f2aa9e8ff5f49d0
crushyna <crushyna@gmail.com>
2019-11-05 21:11:12 +0100

    new classifier in progress

---
+++ number_recognizer.py
@@ -0,0 +1,13 @@
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+
+image_index = 7777  # test image from set
+print(y_train[image_index])     # should be 8
+
+# testing selected image from train set
+plt.imshow(x_train[image_index], cmap='Greys')
+plt.show()
+
+# print(x_train.shape)
