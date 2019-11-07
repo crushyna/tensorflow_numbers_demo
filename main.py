ae2aad30f1dc5eb39fb8a7f03388a0ab26c75945
Radek M <crushyna@gmail.com>
2019-11-07 15:46:39 +0100

    working ALMOST correctly

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import messagebox
 import PIL
 from PIL import Image, ImageDraw
 
-# from number_recognizer import numberRecognition
+from number_recognizer import numberRecognition
 
 # import numpy as np
 
@@ -53,8 +53,8 @@ def paint(event):
     # python_green = "#476042"
     x1, y1 = (event.x - 1), (event.y - 1)
     x2, y2 = (event.x + 1), (event.y + 1)
-    cv.create_oval(x1, y1, x2, y2, fill="black", width=12)
-    draw.line([x1, y1, x2, y2], fill="black", width=12)
+    cv.create_oval(x1, y1, x2, y2, fill="black", width=15)
+    draw.line([x1, y1, x2, y2], fill="black", width=15)
 
 
 root = Tk()
--- number_recognizer_2.py
+++ number_recognizer_2.py
@@ -1,5 +1,3 @@
-from tkinter import Image
-# import keras
 import matplotlib.pyplot as plt
 from matplotlib.image import imread
 import numpy as np
@@ -10,51 +8,68 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation
 from tensorflow.keras.datasets import mnist
 from tensorflow.keras.utils import to_categorical
+from keras.utils.np_utils import to_categorical
 
-# get the MNIST dataset
-# split the dataset into training and testing datasets
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# plot the first 4 images
-'''
-for i in range(1, 5):
-    plt.imshow(x_train[i])
-    plt.show()
-'''
+def train_neural_network():
+    # get the MNIST dataset
+    # split the dataset into training and testing datasets
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-img3 = imread('image.png')
-img4 = np.asarray(Image.open("image.png").convert('L')).ravel()
-img4 = img4.reshape(-1, 784)
-print(img3.shape)
-print(img4.shape)
+    # flatten 28*28 images to a 784 vector for each image
+    num_pixels = x_train.shape[1] * x_train.shape[2]
+    x_train = x_train.reshape((x_train.shape[0], num_pixels)).astype('float32')
+    x_test = x_test.reshape((x_test.shape[0], num_pixels)).astype('float32')
 
-# each matrix in the training/test set needs to be “unrolled”
-x_train = x_train.reshape(-1, 784)
-x_test = x_test.reshape(-1, 784)
+    # normalize inputs from 0-255 to 0-1
+    x_train = x_train / 255
+    x_test = x_test / 255
 
-# convert train and test outputs to one hot encoding
-y_train = to_categorical(y_train, 10)
-y_test = to_categorical(y_test, 10)
+    # plot the first 4 images
+    '''
+    for i in range(1, 5):
+        plt.imshow(x_train[i])
+        plt.show()
+    '''
 
-model = Sequential()
-model.add(Dense(25, input_dim=28 * 28, activation='relu'))
-model.add(Dense(256, activation='relu'))
-model.add(Dense(300, activation='relu'))
-model.add(Dense(10, activation='softmax'))
+    img3 = imread('image.png')
+    print(img3.shape)
 
-# model.compile configures the learning process
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    img4 = np.asarray(Image.open("image.png").convert('L')).ravel()
+    img4 = img4.reshape(-1, 784)
+    print(img4.shape)
 
-# epochs is the number of passes through the training data
-# batch size is the number of samples that are sent through the network
-model.fit(x_train, y_train, epochs=20, shuffle=True, verbose=2, batch_size=128)
+    img5 = np.invert(Image.open("image.png").convert('L')).ravel()
+    img5 = img5.reshape(-1, 784)
+    print(img5.shape)
 
-# run neural network on test data
-test_error_rate = model.evaluate(x_test, y_test, verbose=0)
-print(model.metrics_names)
-print(test_error_rate)
+    # each matrix in the training/test set needs to be “unrolled”
+    x_train = x_train.reshape(-1, 784)
+    x_test = x_test.reshape(-1, 784)
 
-model.save("trainedMNISTModel.h5")
+    # convert train and test outputs to one hot encoding
+    y_train = to_categorical(y_train, 10)
+    y_test = to_categorical(y_test, 10)
+
+    model = Sequential()
+    model.add(Dense(25, input_dim=28 * 28, activation='relu'))
+    model.add(Dense(256, activation='relu'))
+    model.add(Dense(300, activation='relu'))
+    model.add(Dense(10, activation='softmax'))
+
+    # model.compile configures the learning process
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+    # epochs is the number of passes through the training data
+    # batch size is the number of samples that are sent through the network
+    model.fit(x_train, y_train, epochs=20, shuffle=True, verbose=2, batch_size=128)
+
+    # run neural network on test data
+    test_error_rate = model.evaluate(x_test, y_test, verbose=0)
+    print(model.metrics_names)
+    print(test_error_rate)
+
+    model.save("trainedMNISTModel.h5")
 
 '''
 prediction = model.predict(img4)
@@ -72,6 +87,11 @@ img2 = np.asarray(Image.open('image.png').convert('L'))
 '''
 # img3 = imread('image.png')
 
-prediction = model.predict(img4)
-plt.imshow(img4)
-print("Prediction for test image:", np.squeeze(prediction))
+def predict_number(image_of_number):
+    prediction = model.predict(img4)
+    plt.imshow(img4)
+    print("Prediction for test image 1:", np.squeeze(prediction))
+
+    prediction = model.predict(img5)
+    plt.imshow(img5)
+    print("Prediction for test image 2:", np.squeeze(prediction))
